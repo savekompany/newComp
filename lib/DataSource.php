0c18b503d52b5d96c7a7ccfb3e7a8355f45924a7
209854 <amrit.mohapatra1@aa.com>
2020-02-16 20:13:09 -0600

    added cloud mysql details

--- lib/DataSource.php
+++ lib/DataSource.php
@@ -12,13 +12,13 @@ class DataSource
 
     // PHP 7.1.0 visibility modifiers are allowed for class constants.
     // when using above 7.1.0, declare the below constants as private
-    private const HOST = 'localhost';
+    private const HOST = 'sql9.freemysqlhosting.net';
 
-    private const USERNAME = 'root';
+    private const USERNAME = 'sql9323080';
 
-    private const PASSWORD = '';
+    private const PASSWORD = 'fhHBC3LR2h';
 
-    private const DATABASENAME = 'test';
+    private const DATABASENAME = 'sql9323080';
 
     private $conn;
 
